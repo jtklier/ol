be47a02406a2f379d56aa1ba729b41be4ba7bdac
jtklier <jtklier@gmail.com>
2016-08-07 15:21:12 -0700

    added initial capability to parse CSV to JSON
    using csv-parse from npm

---
+++ app.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var csv = require('csv');
+
+var MAXLIMIT = 50;
+var limitCount = 0;
+var page = 40;
+
+var parser = csv.parse({delimiter: ',', columns: true });
+parser.on('readable', function(){
+  while((record = parser.read()) && limitCount<MAXLIMIT){
+    if(parser.count >= page*MAXLIMIT){
+      console.log(record);
+      limitCount++;
+    }
+  }
+});
+
+fs.createReadStream('data/50k_businesses.csv').pipe(parser);
