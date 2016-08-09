2acb770bae044db8416c168b7c2ad067293f668e
jtklier <jtklier@gmail.com>
2016-08-08 23:05:18 -0700

    Added in more error handling/edge cases
    Upon starting the server, the application scans the csv and determines the total number of records, so that the services may more quickly identify out-of-bounds input when handling requests. 
    Added error handling for invalid input parameter values and ID not found. 
    Added functionality for edge case where user requests the last set of data and the result set is less than the max number of records per page.

--- app.js
+++ app.js
@@ -3,41 +3,55 @@ var app = express();
 var fs = require('fs');
 var csv = require('csv');
 
+const DATASOURCE = 'data/50k_businesses.csv';
+var totalNumRecords;
 var output;
 
 app.get('/businesses', function( req, res ){
-  var rs = fs.createReadStream('data/50k_businesses.csv');
+  var rs = fs.createReadStream(DATASOURCE);
     var page = req.query.page || 1;
-    var MAXLIMIT = req.query.recordsPerPage || 50;
-    var offset = (page-1)*MAXLIMIT;
+    var recordsPerPage = req.query.recordsPerPage || 50;
+    var offset = (page-1)*recordsPerPage;
     output = {'page': page, businesses: []};
     res.type('json');
-
     //TODO add error handling here for invalid requests
+    if(isNaN(page) || parseInt(page) < 1){
+      output.message = 'Invalid parameter - Please input a positive integer for the page.';
+      res.status(400).end(JSON.stringify(output));
+    }
+    else if((page*recordsPerPage) > totalNumRecords){
+      output.message = 'Parameter out of bounds - The requested page is out of bounds of the dataset.';
+      res.status(400).end(JSON.stringify(output));
+    }
+    else{
+      var parser = csv.parse({delimiter: ',', columns: true });
 
-
-    var parser = csv.parse({delimiter: ',', columns: true });
-
-    parser.on('readable', function(){
-      while(record = parser.read()){
-        if(output.businesses.length == MAXLIMIT){
-          rs.destroy();
-          res.status(200).end(JSON.stringify(output));
+      parser.on('readable', function(){
+        while(record = parser.read()){
+          if(output.businesses.length == recordsPerPage){
+            rs.destroy();
+            res.status(200).end(JSON.stringify(output));
+          }
+          else if(parser.count == totalNumRecords){
+            output.businesses.push(record);
+            rs.destroy();
+            res.status(200).end(JSON.stringify(output));
+          }
+          else if(parser.count > offset)
+            output.businesses.push(record);
         }
-        else if(parser.count > offset)
-          output.businesses.push(record);
-      }
-    });
+      });
 
-    parser.on('error', function(err){
-      res.status(500).render('error', { message: err });
-    })
-    rs.pipe(parser);
+      parser.on('error', function(err){
+        res.status(500).render('error', { message: err });
+      })
+      rs.pipe(parser);
+    }
 });
 
 
 app.get('/businesses/:id', function( req, res){
-  var rs = fs.createReadStream('data/50k_businesses.csv');
+  var rs = fs.createReadStream(DATASOURCE);
   var index =req.params.id;
   output = {};
   res.type('json');
@@ -50,6 +64,10 @@ app.get('/businesses/:id', function( req, res){
         rs.destroy();
         res.status(200).end(JSON.stringify(output));
       }
+      else if(parser.count >= totalNumRecords){
+        output.message = 'Not found - The requested id was not found: ' + index;
+        res.status(404).end(JSON.stringify(output));
+      }
     }
   });
 
@@ -59,8 +77,17 @@ app.get('/businesses/:id', function( req, res){
   rs.pipe(parser);
 });
 
+function setTotalNumRecords(cb) {
+  var rs = fs.createReadStream(DATASOURCE);
+  var parser = csv.parse({delimiter: ',', columns: true }, function(err, data){
+    totalNumRecords = data.length;
+    cb();
+  });
+  rs.pipe(parser);
+}
+
 var server = app.listen(8081, function() {
   var host = server.address().address;
   var port = server.address().port;
-  console.log("Now listening at "+host+":"+port);
+  setTotalNumRecords(function() {console.log("Now listening at "+host+":"+port);});
 });
